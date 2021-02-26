53f5da502b50849e7695b05045683e10a5c1c999
Nemika <33802693+Nemijus@users.noreply.github.com>
2021-02-26 11:07:48 +0200

    Update thanking.py

--- cogs/thanking.py
+++ cogs/thanking.py
@@ -7,6 +7,13 @@ commands = discord.ext.commands
 class Thanking(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.allowed = [
+            807226206486265886,
+            807227119884959766,
+            807227628511428608,
+            807227983118467072,
+            807228592751902760
+        ]
 
     
     @commands.Cog.listener()
@@ -14,7 +21,7 @@ class Thanking(commands.Cog):
         if not message.guild or message.author.bot: return
         if not message.guild.id == files.Data("config").yaml_read()['server']: return
         if not message.channel.category: return
-        if not message.channel.category.id == 814176201924673566: return
+        if not message.channel.category.id in self.allowed: return
 
         if (message.content.lower().startswith("thanks ") or message.content.lower().startswith("thank ")) and message.mentions:
             return await message.channel.send(embed=embeds.Embeds(f"{message.author.mention} thanked " + ', '.join(member.mention for member in message.mentions)).success())
