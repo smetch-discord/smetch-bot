454e934b979588cde1ce5ef434a417944faa1fb6
Nemika <33802693+Nemijus@users.noreply.github.com>
2021-02-26 12:38:19 +0200

    Updated thanking

--- core/database.py
+++ core/database.py
@@ -7,8 +7,8 @@ client = MongoClient(Data("config").yaml_read()['mongo-uri'])
 class Punishments:
     def __init__(self, user_id=None):
         self.user_id = user_id
-        self.db = client["main"]
-        self.col = client["main"]["punishments"]
+        self.db = client["SMETCH"]
+        self.col = client["SMETCH"]["punishments"]
 
     
     def getCases(self, **kwargs):
@@ -28,3 +28,48 @@ class Punishments:
     
     def removeCase(self, **kwargs):
         self.col.delete_one(kwargs)
+
+class Thanks:
+    def __init__(self, user_id=None):
+        self.user = user_id
+        self.col = client["SMETCH"]["thanks"]
+        if self.user:
+            if not self.col.find_one({"_id":self.user}):
+                self.col.insert_one({"_id": self.user, "thanks_daily": 0, "thanks_weekly":0, "thanks_alltime":0})
+    
+    @property
+    def exists(self):
+        r = self.col.find_one({"_id":self.user})
+        if r: return True
+        return False
+    
+    @property
+    def delete(self):
+        self.col.delete_one({"_id":self.user})
+
+    @property
+    def reset_weekly(self):
+        self.col.update_many({}, {"$set": {"thanks_weekly": 0}})
+
+    @property
+    def reset_daily(self):
+        self.col.update_many({}, {"$set": {"thanks_daily": 0}})
+
+    def add(self, points=1):
+        self.col.update_one({"_id":self.user},
+        {"$inc": {
+            "thanks_daily": points,
+            "thanks_weekly": points,
+            "thanks_alltime": points
+        }})
+
+    def remove(self, points=1):
+        self.col.update_one({"_id":self.user},
+        {"$inc": {
+            "thanks_daily": -points,
+            "thanks_weekly": points,
+            "thanks_alltime": points
+        }})
+    
+    def get(self):
+        return self.col.find_one({"_id":self.user})
