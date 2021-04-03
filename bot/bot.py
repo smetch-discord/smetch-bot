a1013f07946fdb68587c511b050cf74eba5fca41
Vivaan Verma <vivaan.verma@gmail.com>
2021-04-03 22:45:52 +0100

    Finished logging set up

--- bot/bot.py
+++ bot/bot.py
@@ -1,19 +1,19 @@
 from constants import BOT_TOKEN
-import logging
 from discord.ext import commands
+import logging
+from log_setup import log_setup
+log_setup()
+
 
 # Filter out info to include warnings and above in order to not clog bot.log
 discord_log = logging.getLogger('discord')
 discord_log.setLevel(logging.WARNING)
+asyncio_log = logging.getLogger('asyncio')
+asyncio_log.setLevel(logging.WARNING)
 
 # Set up logging
-logging.basicConfig(
-    filename='bot.log',
-    filemode='w',
-    format='%(name)s: %(levelname)s - %(message)s',
-    level=logging.DEBUG
-)
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 # Initialise bot
 bot = commands.Bot(command_prefix='s!')
@@ -21,11 +21,12 @@ bot = commands.Bot(command_prefix='s!')
 
 @bot.event
 async def on_ready():
-    log.info('Bot is running')
+    logger.info('Bot is running')
 
 
 @bot.command(name='test')
 async def test(ctx):
-    print('This is a test command')
+    await ctx.send('This is a test command')
 
+# Run the bot
 bot.run(BOT_TOKEN)
--- bot/constants.py
+++ bot/constants.py
@@ -3,14 +3,23 @@ import io
 import logging
 
 log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 # Load all configuration constants from file
-config_file: io.TextIOWrapper = io.open('config.yml')
-log.info('Successfully opened the config.yaml file')
+try:
+    config_file: io.TextIOWrapper = io.open('config.yml')
+    log.info('Successfully opened the config.yml file')
+except FileNotFoundError:
+    log.critical(
+        "No config.yml file was found. \
+        Please make sure the file is called 'config.yml'. \
+        Make sure it is in the top level directory."
+    )
+    log.exception('Missing config.yml found')
 
 # Parse YAML file
 config: dict = yaml.safe_load(config_file)
-log.info('Successfully parsed the config.yaml file')
+log.info('Successfully parsed the config.yml file')
 
 # Define constants for use in the bot
 BOT_TOKEN: str = config.__getitem__('bot-token')
