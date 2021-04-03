4c4f826adb3561e1ecdd6332ad104e8906fa12db
Vivaan Verma <vivaan.verma@gmail.com>
2021-04-03 18:33:23 +0100

    Added logging

--- bot/bot.py
+++ bot/bot.py
@@ -1,12 +1,27 @@
-from discord.ext import commands
 from constants import BOT_TOKEN
+import logging
+from discord.ext import commands
 
+# Filter out info to include warnings and above in order to not clog bot.log
+discord_log = logging.getLogger('discord')
+discord_log.setLevel(logging.WARNING)
+
+# Set up logging
+logging.basicConfig(
+    filename='bot.log',
+    filemode='w',
+    format='%(name)s: %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+log = logging.getLogger(__name__)
+
+# Initialise bot
 bot = commands.Bot(command_prefix='s!')
 
 
 @bot.event
 async def on_ready():
-    print('Bot is running')
+    log.info('Bot is running')
 
 
 @bot.command(name='test')
--- bot/constants.py
+++ bot/constants.py
@@ -1,13 +1,19 @@
 import yaml
 import io
+import logging
+
+log = logging.getLogger(__name__)
 
 # Load all configuration constants from file
-config_file: io.TextIOWrapper = io.open('config.yml')
+config_file: io.TextIOWrapper = io.open('config.yaml')
+log.info('Successfully openeed the config.yaml file')
 
 # Parse YAML file
 config: dict = yaml.safe_load(config_file)
+log.info('Successfully parsed the config.yaml file')
 
 # Define constants for use in the bot
 BOT_TOKEN: str = config.__getitem__('bot-token')
 MONGO_URI: str = config.__getitem__('mongo-uri')
 GITHUB_TOKEN: str = config.__getitem__('github-token')
+log.info('Loaded all constants')
