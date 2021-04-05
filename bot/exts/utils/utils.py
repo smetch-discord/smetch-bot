d5dea6b148a286c2142147a860d6e8c002179cfb
Vivaan Verma <vivaan.verma@gmail.com>
2021-04-05 14:13:09 +0100

    utils continued

--- bot/exts/utils/utils.py
+++ bot/exts/utils/utils.py
@@ -1,32 +1,44 @@
 
 from pathlib import Path
 from discord import Embed
-from discord.ext.commands import Cog
-from bot.constants import constants
+from discord.ext.commands import Cog, Command
 
 
 def load_util_files():
     resource_folder = Path('.', 'bot', 'resources')
     util_files = list(resource_folder.glob('*.md'))
-    util_files = list(util_files.map(lambda file: open(file, 'r').read()))
+    util_files = {file.name[:-3]: open(file).read() for file in util_files}
     return util_files
 
 
 def markdown_to_embed(markdown: str):
-    embed = Embed
+    embed = Embed()
     title = markdown.split('\n')[0].strip('*')
     embed.title = title
     content = '\n'.join(markdown.split('\n')[1:])
     embed.description = content
-    embed.color = constants.color.white
     return embed
 
 
-def create_command(embed: Embed):
-    pass
+def create_command(name: str, embed: Embed):
+
+    async def command_template(ctx):
+        await ctx.send(embed=embed)
+
+    return Command(
+        command_template,
+        name=name,
+        cog=Utility
+    )
 
 
 class Utility(Cog):
 
-    def __init__(self):
-        pass
+    def __init__(self, bot):
+        self.bot = bot
+        util_commands_dict = load_util_files()
+        for command_name in util_commands_dict:
+            command_embed = markdown_to_embed(util_commands_dict[command_name])
+            command = create_command(command_name, command_embed)
+            self.bot.add_command(command)
+        print(self.get_commands())
