c956daf994d5c9bcb0898ca739b9ca3525b1e0a7
vivaanverma23 <>
2021-04-13 08:10:48 +0000

    Finish ping started server

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 from discord.ext.commands import Bot, Context
 from discord import Embed
 from datetime import datetime
+from yaml import safe_load
 
 bot: Bot = Bot('lol ', help_command=None)
 
@@ -12,7 +13,8 @@ async def ping(ctx: Context):
     discord_ping: str = f'{bot.latency * 1000:.3f} ms'
 
     embed: Embed = Embed(
-        title='Ping!'
+        title='Ping!',
+        colour=0x00d166
     )
 
     for description, latency in zip(['Command Processing Time', 'Discord API Latency'], [bot_ping, discord_ping]):
@@ -25,4 +27,44 @@ async def ping(ctx: Context):
     await ctx.send(embed=embed)
     return
 
-bot.run('ODA4MjgyNzEyMzY3NTYyNzYy.YCER7w.bRD1xhdUtJWdcI5TYhJ9CkVoJ7Q')
+
+@bot.command()
+async def server(ctx: Context):
+    guild = ctx.message.guild
+    embed = Embed(
+        title=guild.name,
+        description='SMETCH is a community that helps those in need!',
+        colour=0x00d166
+    )
+
+    embed.set_thumbnail(
+        url=guild.icon_url
+    )
+
+    embed.add_field(
+        name='Server was created on:',
+        value=guild.created_at.strftime('%d %B %Y'),
+        inline=False
+    )
+
+    embed.add_field(
+        name='Member count:',
+        value=guild.member_count,
+        inline=False
+    )
+
+    embed.add_field(
+        name='Current owner:',
+        value=f'<@{guild.owner_id}>'
+    )
+
+    embed.add_field(
+        name='Current boosts:',
+        value=guild.premium_subscription_count
+    )
+
+    await ctx.send(embed=embed)
+
+config = safe_load(open('config.yml'))
+
+bot.run(config['bot-token'])
