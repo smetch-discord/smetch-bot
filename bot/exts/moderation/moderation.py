6a2787eb88f059b67947f717901e23f1fa09ec37
Vivaan Verma <vivaan.verma@gmail.com>
2021-04-15 14:30:35 +0100

    Added kick cmd

--- bot/exts/moderation/moderation.py
+++ bot/exts/moderation/moderation.py
@@ -1,9 +1,10 @@
+from discord.embeds import Embed
 from discord.ext.commands import Bot, Cog, Context, command
 from discord.ext.commands.converter import Greedy
-from discord import Member, Embed
+from discord import Member
 from typing import Optional
 
-from utils.checks import can_ban
+from utils.checks import can_ban, can_kick
 
 
 class Moderation(Cog):
@@ -22,14 +23,43 @@ class Moderation(Cog):
     async def test(self, ctx: Context):
         await ctx.send('Test returned')
 
+    @staticmethod
+    def generate_confirmation_embeds(type_: str, user: Member, reason: str) -> tuple[Embed, Embed]:
+        pass
+
     @command(description='Bans a user')
     @can_ban()
-    async def ban(self, ctx: Context, users: Greedy[Member], reason: str = 'None was provided') -> None:
+    async def ban(self, ctx: Context, users: Greedy[Member], *reason: Optional[tuple[str]]) -> None:
+        '''
+        Bans a user after checking for the correct permissions
+        Makes sure that the user is either:
+        1. An administrator
+        2. A moderator
+        It also prevents anybody from banning themselves
+        '''
+        # Each word in the reason is provided as a separate string, concatenate them together
+        reason = ' '.join([word for word in reason]) if reason else 'No reason was provided'
         for user in users:
+            # Make sure the user is not banning themselves or somebody higher than them in the role hierarchy
             if user.id == ctx.author.id or user.top_role >= ctx.author.top_role:
                 continue
             await ctx.guild.ban(
                 user=user,
                 reason=f'Banned by {ctx.author.name}#{ctx.author.discriminator} for reason: {reason}'
+            )
+        return
+
+    @command(description='Kicks a user')
+    @can_kick()
+    async def kick(self, ctx: Context, users: Greedy[Member], *reason: Optional[tuple[str]]) -> None:
+        # Each word in the reason is provided as a separate string, concatenate them together
+        reason = ' '.join([word for word in reason]) if reason else 'No reason was provided'
+        for user in users:
+            # Make sure the user is not kicking themselves or somebody higher than them in the role hierarchy
+            if user.id == ctx.author.id or user.top_role >= ctx.author.top_role:
+                continue
+            await ctx.guild.kick(
+                user=user,
+                reason=f'Kicked by {ctx.author.name}#{ctx.author.discriminator} for reason: {reason}'
             )
         return
--- bot/utils/checks.py
+++ bot/utils/checks.py
@@ -13,7 +13,26 @@ def can_ban():
     def predicate(ctx: Context) -> bool:
         mod_role: Role = get(ctx.guild.roles, id=807229822799446036)
         admin_role: Role = get(ctx.guild.roles, id=806922773649555469)
-        if mod_role in ctx.author.roles or admin_role in ctx.author.roles:
+        if any([role in ctx.author.roles for role in (mod_role, admin_role)]):
+            return True
+        else:
+            return False
+    return check(predicate)
+
+
+def can_kick():
+    '''
+    Checks if a user has kick priveliges. Only people with:
+    1. The administrator role
+    2. The moderator role
+    3. The trainee moderator role
+    Should be able to kick.
+    '''
+    def predicate(ctx: Context) -> bool:
+        trainee_mod_role: Role = get(ctx.guild.roles, id=807410624438861875)
+        mod_role: Role = get(ctx.guild.roles, id=807229822799446036)
+        admin_role: Role = get(ctx.guild.roles, id=806922773649555469)
+        if any([role in ctx.author.roles for role in (trainee_mod_role, mod_role, admin_role)]):
             return True
         else:
             return False
