3e1735bbda5fe6618869f460ddf1fd2cf91eed15
Vivaan Verma <vivaan.verma@gmail.com>
2021-04-30 22:57:30 +0100

    Secrets update

--- bot/constants.py
+++ bot/constants.py
@@ -1,7 +1,8 @@
+from typing import Dict
+
 from yaml import safe_load
 from logging import getLogger, Logger, DEBUG
 from pathlib import Path
-from dataclasses import dataclass
 
 log: Logger = getLogger(__name__)
 log.setLevel(DEBUG)
@@ -23,6 +24,23 @@ class Secrets:
             log.critical("No secrets field was located in config")
             raise KeyError("No secrets key found")
         secrets: dict = config["secrets"]
-        self.token: str = secrets.get("bot-token")
-        self.mongo_uri: str = secrets.get("mongo-uri")
 
+        self.bot_token: str = secrets.get("bot-token")
+        self.mongo_uri: str = secrets.get("mongo-uri")
+        self.github_token: str = secrets.get("github-token")
+
+        pretty_print_names: dict[str, str] = {
+            "bot-token": "Discord bot token",
+            "mongo-uri": "MongoDB connection URI",
+            "github-token": "GitHub token"
+        }
+
+        for secret in (self.bot_token, self.mongo_uri, self.github_token):
+            if secret is None:
+                secret_name: str = f'{secret=}'.split("=")[0]
+                secret_name: str = pretty_print_names[secret_name]
+                log.critical(f'No {secret_name} was found')
+
+    @staticmethod
+    def bot_token_check(token: str):
+        pass
